4a289a3cc0e5cf4aed362fa962bd398ee9219afe
mccartyzzz <mccarty_zzz2017@163.com>
2020-07-05 15:40:24 +0800

    check

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 const Grid=require("./ui/grid");
+const PopupNumbers = require('./ui/popupnumbers');
 
 //九宫格的显示
 const grid = new Grid($("#container"));
 grid.build();
 grid.layout();
 
-
+//创建一个popupNumbers对象，绑定弹出窗口
+const popupNumbers = new PopupNumbers($("#popupNumbers"));
+grid.bindPopup(popupNumbers);
