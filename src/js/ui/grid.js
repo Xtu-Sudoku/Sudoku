49051d65bd8107ceaa640df98715e889ae84802e
mccartyzzz <mccarty_zzz2017@163.com>
2020-07-05 14:46:45 +0800

    v5

--- src/js/ui/grid.js
+++ src/js/ui/grid.js
@@ -1,46 +1,60 @@
 const Toolkit = require("../core/toolkit");
-
+const Sudoku = require("../core/sudoku");
+const Generator = require("../core/generator");
 //九宫格生成在container中
 //生成九宫格
-class Grid{
-    constructor(container){
+class Grid {
+    constructor(container) {
         this._$container = container;
     }
-    build(){
-    	const matrix = Toolkit.matrix.makeMatrix();
-    	
-        const rowGroupClasses = ["row_g_top","row_g_middle","row_g_bottom"];
-        const colGroupClasses = ["col_g_left","col_g_center","col_g_right"];
-        
+    build() {
+        const sudoku = new Sudoku();
+        sudoku.make();
+        const matrix = sudoku.puzzleMatrix;
+
+        //  	const matrix = Toolkit.matrix.makeMatrix();
+
+        const rowGroupClasses = ["row_g_top", "row_g_middle", "row_g_bottom"];
+        const colGroupClasses = ["col_g_left", "col_g_center", "col_g_right"];
+
         //每一行创建一个div将每一行的值new成一个span
         const $cells = matrix.map(rowValues => rowValues
-            .map((cellValue,colIndex)=> {
+            .map((cellValue, colIndex) => {
                 return $("<span>")
-                .addClass(colGroupClasses[colIndex % 3])
-                .text(cellValue);
+                    .addClass(colGroupClasses[colIndex % 3])
+                    .addClass(cellValue ? "fixed" : "empty")//迷盘添加颜色
+                    .text(cellValue);
             }));
         //从cells中得到div数组
-        const $divArray = $cells.map(($spanArray,rowIndex) => {
+        const $divArray = $cells.map(($spanArray, rowIndex) => {
             return $("<div>")
-            .addClass("row")
-            .addClass(rowGroupClasses[rowIndex % 3])
-            .append($spanArray);
+                .addClass("row")
+                .addClass(rowGroupClasses[rowIndex % 3])
+                .append($spanArray);
         });
         //添加到container中
         this._$container.append($divArray);
     }
-    layout(){
-        const width = $("span:first",this._$container).width();
-        $("span",this._$container)
+    layout() {
+        const width = $("span:first", this._$container).width();
+        $("span", this._$container)
             .height(width)
             .css({
-                "line-height":`${width}px`,
-                "font-size":width<32 ? `${width / 2 }`:""
+                "line-height": `${width}px`,
+                "font-size": width < 32 ? `${width / 2}` : ""
             });
     }
 
-    
+
 }
+
+const generator = new Generator();
+generator.generate();
+// 		console.log(generator.matrix);
+
+const matrix = generator.matrix;
+console.log("matrix\n", matrix);
+
 
 //new Grid($("#container")).build();
 
--- www/js/index.js
+++ www/js/index.js
@@ -59,48 +59,66 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 	const Toolkit = __webpack_require__(2);
-	
+	const Generator = __webpack_require__(3);
 	//九宫格生成在container中
 	//生成九宫格
-	class Grid{
-	    constructor(container){
+	class Grid {
+	    constructor(container) {
 	        this._$container = container;
 	    }
-	    build(){
-	    	const matrix = Toolkit.matrix.makeMatrix();
-	    	
-	        const rowGroupClasses = ["row_g_top","row_g_middle","row_g_bottom"];
-	        const colGroupClasses = ["col_g_left","col_g_center","col_g_right"];
-	        
+	    build() {
+	        const generator = new Generator();
+	        generator.generate();
+	        const solutionMatrix = generator.matrix;
+	        var level = 5;
+	        const pazzleMatrix = solutionMatrix.map(row => {
+	            return row.map(cell => Math.random() * 9 < level ? 0 : cell);
+	        })
+	
+	        const matrix = pazzleMatrix;
+	        //  	const matrix = Toolkit.matrix.makeMatrix();
+	
+	        const rowGroupClasses = ["row_g_top", "row_g_middle", "row_g_bottom"];
+	        const colGroupClasses = ["col_g_left", "col_g_center", "col_g_right"];
+	
 	        //每一行创建一个div将每一行的值new成一个span
 	        const $cells = matrix.map(rowValues => rowValues
-	            .map((cellValue,colIndex)=> {
+	            .map((cellValue, colIndex) => {
 	                return $("<span>")
-	                .addClass(colGroupClasses[colIndex % 3])
-	                .text(cellValue);
+	                    .addClass(colGroupClasses[colIndex % 3])
+	                    .addClass(cellValue ? "fixed" : "empty")//迷盘添加颜色
+	                    .text(cellValue);
 	            }));
 	        //从cells中得到div数组
-	        const $divArray = $cells.map(($spanArray,rowIndex) => {
+	        const $divArray = $cells.map(($spanArray, rowIndex) => {
 	            return $("<div>")
-	            .addClass("row")
-	            .addClass(rowGroupClasses[rowIndex % 3])
-	            .append($spanArray);
+	                .addClass("row")
+	                .addClass(rowGroupClasses[rowIndex % 3])
+	                .append($spanArray);
 	        });
 	        //添加到container中
 	        this._$container.append($divArray);
 	    }
-	    layout(){
-	        const width = $("span:first",this._$container).width();
-	        $("span",this._$container)
+	    layout() {
+	        const width = $("span:first", this._$container).width();
+	        $("span", this._$container)
 	            .height(width)
 	            .css({
-	                "line-height":`${width}px`,
-	                "font-size":width<32 ? `${width / 2 }`:""
+	                "line-height": `${width}px`,
+	                "font-size": width < 32 ? `${width / 2}` : ""
 	            });
 	    }
 	
-	    
+	
 	}
+	
+	const generator = new Generator();
+	generator.generate();
+	// 		console.log(generator.matrix);
+	
+	const matrix = generator.matrix;
+	console.log("matrix\n", matrix);
+	
 	
 	//new Grid($("#container")).build();
 	
@@ -198,6 +216,74 @@
 	    }
 	    
 	};
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	//生成数独解决方案
+	
+	const Toolkit = __webpack_require__(2);
+	
+	module.exports = class Generator{//要导出
+	//class Generator{
+	    generate(){
+	        while(!this.internalGenerate()){
+	            console.warn("try again");
+	        }
+	    }
+	    internalGenerate(){
+	        // 入口方法
+	        this.matrix = Toolkit.matrix.makeMatrix();
+	        this.orders = Toolkit.matrix.makeMatrix()
+	            .map(row => row.map((v,i)=> i))//每一行，0-8，随机序列
+	            .map(row => Toolkit.matrix.shuffle(row));//洗牌方法，打乱，随机选择
+	            
+	            for (let n=1;n<=9;n++){
+	            	if(!this.fillNumber(n)){
+	               		return false;
+	            	}
+	        	}
+	        return true;
+	    }
+	
+	    fillNumber(n){//填充行
+	        return this.fillRow(n,0);
+	    }
+	    //递归
+	    fillRow(n,rowIndex){
+	        if(rowIndex > 8){//结束
+	            return true;
+	        }
+	        //行数据
+	        const row = this.matrix[rowIndex];
+	        //选择填写位置
+	        const orders = this.orders[rowIndex];
+	        for(let i = 0 ;i<9; i++){ 
+		        const colIndex =orders[i];//固定
+	            //判断数据 如果此位置已有值，跳过
+	            if(row[colIndex]){
+	                continue;
+	            }
+	            //检查此位置是否能填
+	            if(!Toolkit.matrix.checkFillable(this.matrix,n,rowIndex,colIndex)){
+	                continue;
+	            }
+	            row[colIndex] = n;
+	            //去下一行填写n 如果填写失败则继续寻找当前行下一个位置
+	            if(!this.fillRow(n,rowIndex + 1)){
+	                row[colIndex] = 0;
+	                continue;
+	            }
+	            return true;
+	        }
+	        return false;
+	    }
+	}
+	
+	// const generator = new Generator();
+	// generator.generate();
+	// console.log(generator.matrix);
 
 /***/ })
 /******/ ]);
