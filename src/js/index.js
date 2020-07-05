7674195c81cf10b42563f99857873f1ea4d5d73f
mccartyzzz <mccarty_zzz2017@163.com>
2020-07-05 15:47:46 +0800

    version 9

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,26 @@ grid.layout();
 //创建一个popupNumbers对象，绑定弹出窗口
 const popupNumbers = new PopupNumbers($("#popupNumbers"));
 grid.bindPopup(popupNumbers);
+
+
+$("#check").on("click", e => {
+    popupNumbers.hide();
+    if (grid.check()) {
+        alert("Congruations!");
+    }
+});
+
+$("#reset").on("click", e => {
+    popupNumbers.hide();
+    grid.reset();
+});
+
+$("#clear").on("click", e => {
+    popupNumbers.hide();
+    grid.clear();
+});
+
+$("#rebuild").on("click", e => {
+    popupNumbers.hide();
+    grid.rebuild();
+});
